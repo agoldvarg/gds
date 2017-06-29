51f806dae7048f078bf50bf65bee89da64b93d72
Alex Goldvarg <agoldvarg@gmail.com>
2017-06-29 08:21:13 -0400

    Import Strings lib and fix some basic behaviors

--- contracts/Skywalker.sol
+++ contracts/Skywalker.sol
@@ -1,22 +1,29 @@
 pragma solidity ^0.4.11;
 
+import "./Strings.sol";
+
 contract Skywalker {
+    using Strings for *;
+
     address public admin;
     uint numFlights;
     uint numSeats;
     mapping (string => Flight) flights;
+    string[6] COLUMNS = ["A", "B", "C", "D", "E", "F"];
 
     struct Seat {
         uint id;
         string flightNumber;
         string group;
         address owner;
+        string displayName;
     }
 
     struct Flight {
         uint id;
         string flightNumber;
         address operatingCarrierAddress;
+        uint[] seatIds;
         mapping (uint => Seat) seats;
     }
 
@@ -30,7 +37,8 @@ contract Skywalker {
         flights[_flightNumber] = Flight({
             id: numFlights + 1,
             flightNumber: _flightNumber,
-            operatingCarrierAddress: msg.sender
+            operatingCarrierAddress: msg.sender,
+            seatIds: new uint[](0)
         });
 
         numFlights = numFlights + 1;
@@ -38,23 +46,31 @@ contract Skywalker {
         return true;
     }
 
-    function addSeatingGroupToFlight(string _flightNumber, string _group, uint _numSeats) returns (bool) {
-        for (uint i = 0; i < _numSeats; i++) {
-            createSeat(_flightNumber, _group);
+    function addSeatingGroupToFlight(string _flightNumber, string _group, uint _rows) returns (bool) {
+        for (uint i = 0; i < _rows; i++) {
+            createSeat(_flightNumber, _group, COLUMNS[i]);
+            createSeat(_flightNumber, _group, COLUMNS[i + 1]);
+            createSeat(_flightNumber, _group, COLUMNS[i + 2]);
+            createSeat(_flightNumber, _group, COLUMNS[i + 3]);
+            createSeat(_flightNumber, _group, COLUMNS[i + 4]);
+            createSeat(_flightNumber, _group, COLUMNS[i + 5]);
         }
 
         return true;
     }
 
-    function createSeat(string _flightNumber, string _group) internal returns (bool) {
+    function createSeat(string _flightNumber, string _group, string _displayName) internal returns (bool) {
         Flight flight = flights[_flightNumber];
 
         flight.seats[numSeats + 1] = Seat({
             id: numSeats + 1,
             flightNumber: _flightNumber,
             group: _group,
-            owner: msg.sender
+            owner: msg.sender,
+            displayName: _displayName,
         });
+
+        flight.seatIds.push(numSeats + 1);
 
         numSeats = numSeats + 1;
 
@@ -69,5 +85,23 @@ contract Skywalker {
             flight.flightNumber,
             flight.operatingCarrierAddress
         );
+    }
+
+    function getSeatsForFlight(string _flightNumber) public constant returns (string) {
+        Flight flight = flights[_flightNumber];
+        string memory seats;
+
+        for (uint i; i < flight.seatIds.length; i++) {
+            seats = seats.toSlice().concat(flight.seats[flight.seatIds[i]].displayName.toSlice());
+            seats = seats.toSlice().concat(",".toSlice());
+        }
+
+        return seats;
+    }
+
+    function getNumberOfSeatsForFlight(string _flightNumber) public constant returns (uint) {
+        Flight flight = flights[_flightNumber];
+
+        return flight.seatIds.length;
     }
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,12 @@
-var ConvertLib = artifacts.require("./ConvertLib.sol");
-var MetaCoin = artifacts.require("./MetaCoin.sol");
+// var ConvertLib = artifacts.require("./ConvertLib.sol");
+// var MetaCoin = artifacts.require("./MetaCoin.sol");
+var Strings = artifacts.require("./Strings.sol");
+var Skywalker = artifacts.require("./Skywalker.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
+  // deployer.deploy(ConvertLib);
+  // deployer.link(ConvertLib, MetaCoin);
+  deployer.deploy(Strings);
+  // deployer.link(Strings, Skywalker);
+  deployer.deploy(Skywalker);
 };
